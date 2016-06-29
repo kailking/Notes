35335fe136dbdcb2aa9043ceb4de14e21143129f
charlie127 <charlie.cui127@gmail.com>
2016-06-29 14:20:05 +0800

    mod

--- Python/Python_Scripts/nettop.py
+++ Python/Python_Scripts/nettop.py
@@ -13,6 +13,7 @@ line_num = 1
 
 # function of get cpu state
 
+
 def getCPUstate(interval=1):
     return (" CPU: " + str(psutil.cpu_percent(interval)) + "%")
 
@@ -22,13 +23,14 @@ def getCPUstate(interval=1):
 def getMemorystate():
     phymem = psutil.virtual_memory()
     line = "Memroy: %5s%% %6s %s" % (
-            phymem.percent,
-            str(int(phymem.used/1024/1024))+"M",
-            str(int(phymem.total/1024/1024))+"M"
-            )
+        phymem.percent,
+        str(int(phymem.used/1024/1024))+"M",
+        str(int(phymem.total/1024/1024))+"M"
+    )
     return line
 
 # bytes 2 human
+
 
 def bytes2human(n):
     """
@@ -64,6 +66,7 @@ def poll(interval):
 
 # refresh window
 
+
 def refresh_window(total_before, total_after, pnic_before, pnic_after, cpu_state, memory_state):
     if os.name == 'nt':
         os.system("cls")
@@ -71,7 +74,7 @@ def refresh_window(total_before, total_after, pnic_before, pnic_after, cpu_state
         os.system("clear")
 
     # print currnet time
-    print time.asctime() + " | " + cpu_state + " | " + memory_state
+    print (time.asctime() + " | " + cpu_state + " | " + memory_state)
 
     # totals
     print 'NetStates:'
@@ -81,40 +84,40 @@ def refresh_window(total_before, total_after, pnic_before, pnic_after, cpu_state
     # per-network interface details: let's sort network interface so that that the ones which
     # generated more traffic are shown first
     print ""
-    nic_names=pnic_after.keys()
+    nic_names = pnic_after.keys()
     # nic_name.sort(key=lambda x: sum(pnic_after[x], reverse=True))
     for name in nic_names:
-        stats_before=pnic_before[name]
-        stats_after=pnic_after[name]
-        templ="%-15s %15s %15s"
+        stats_before = pnic_before[name]
+        stats_after = pnic_after[name]
+        templ = "%-15s %15s %15s"
         print templ % (name, "Total", "PER_SEC")
         print templ % (
-                "bytes_sent", bytes2human(stats_after.bytes_sent),
-                bytes2human(
-                    stats_after.bytes_sent - stats_before.bytes_sent) + '/s'
-            )
+            "bytes_sent", bytes2human(stats_after.bytes_sent),
+            bytes2human(
+                stats_after.bytes_sent - stats_before.bytes_sent) + '/s'
+        )
         print templ % (
-                "bytes_recv", bytes2human(stats_after.bytes_recv),
-                bytes2human(
-                    stats_after.bytes_recv - stats_before.bytes_recv) + '/s'
-            )
+            "bytes_recv", bytes2human(stats_after.bytes_recv),
+            bytes2human(
+                stats_after.bytes_recv - stats_before.bytes_recv) + '/s'
+        )
         print templ % (
-                "packets_sent", stats_after.packets_sent,
-                stats_after.packets_sent - stats_before.packets_sent
-            )
+            "packets_sent", stats_after.packets_sent,
+            stats_after.packets_sent - stats_before.packets_sent
+        )
         print templ % (
-                "packets_recv", stats_after.packets_recv,
-                stats_after.packets_recv - stats_before.packets_recv
-            )
+            "packets_recv", stats_after.packets_recv,
+            stats_after.packets_recv - stats_before.packets_recv
+        )
         print " "
 
 if __name__ == '__main__':
 
     try:
-        interval=0
+        interval = 0
         while True:
             args = poll(interval)
             refresh_window(*args)
-            interval=1
+            interval = 1
     except (KeyboardInterrupt, SystemExit):
-            pass
+        pass
